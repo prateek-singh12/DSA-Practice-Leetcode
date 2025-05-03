0d2797a559f95b2aed5d8c3b4499078d6013c533
prateek-singh12 <146692886+prateek-singh12@users.noreply.github.com>
2025-05-03 18:27:44 +0530

    Time: 6 ms (37.93%), Space: 115.3 MB (76.51%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        int res = getRotation(tops, bottoms, tops[0]);
-        if (tops[0] != bottoms[0]) {
-            res = min(res, getRotation(tops, bottoms, bottoms[0]));
+        //using hashmap
+        vector<int> hash(7,0);
+        for(int i=0;i<tops.size();i++){
+            hash[tops[i]]++;
+            if(tops[i]!=bottoms[i]){ //distinct values
+                hash[bottoms[i]]++;
+            }
+        }
+        
+        int res=INT_MAX;
+        for(int i=1;i<=6;i++){
+            if(hash[i]==tops.size()){
+                int swaptop=0;
+                int swapbottom=0;
+                for(int j=0;j<tops.size();j++){
+                    // if (tops[j] != i && bottoms[j] != i) {
+                    //     swaptop = INT_MAX;
+                    //     break;
+                    // }
+                    if(tops[j]!=i){
+                        swaptop++;
+                    }
+                    else if(bottoms[j]!=i){
+                        swapbottom++;
+                    }
+                }
+                res=min(res,min(swapbottom,swaptop));
+            }
         }
         return res == INT_MAX ? -1 : res;
     }
-
-    int getRotation(vector<int>& tops, vector<int>& bottoms, int target) {
-        int rotateTop = 0;
-        int rotateBottom = 0;
-
-        for (int i = 0; i < tops.size(); i++) {
-            if (tops[i] != target && bottoms[i] != target) {
-                return INT_MAX;
-            }
-            if (tops[i] != target) rotateTop++;
-            if (bottoms[i] != target) rotateBottom++;
-        }
-        return min(rotateTop, rotateBottom);
-    }
-
 };
