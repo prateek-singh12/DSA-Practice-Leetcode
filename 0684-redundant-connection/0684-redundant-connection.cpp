f802b0850a8acd14c2a7f1c0a8348f0d6c73efaf
prateek-singh12 <146692886+prateek-singh12@users.noreply.github.com>
2025-05-27 00:31:27 +0530

    Time: 7 ms (17.21%), Space: 14.5 MB (15.75%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,45 +1,41 @@
-
-using namespace std;
-
 class Solution {
 public:
-    bool dfs(unordered_map<int, vector<int>> &adj, int u, int v, vector<bool> &visited) {
-        visited[u] = true;
 
-        if (u == v) {
+    bool dfs(unordered_map<int , vector<int>> &adj, int u, int v, vector<bool> &visited){
+        visited[u]=1;
+        if(u==v){
             return true;
         }
-
-        for (int &ngbr : adj[u]) {
-            if (visited[ngbr]) continue;
-
-            if (dfs(adj, ngbr, v, visited)) {
+        for(auto &it: adj[u]){
+            if(visited[it]){
+                continue;
+            }
+            if(dfs(adj,it,v,visited)){
                 return true;
             }
         }
-
         return false;
     }
 
-    vector<int> findRedundantConnection(vector<vector<int>> &edges) {
-        int n = edges.size();  // number of nodes = n
-                               // number of edges = n
-
-        unordered_map<int, vector<int>> adj;
-
-        for (int i = 0; i < n; i++) {  // O(n)
-            int u = edges[i][0];
-            int v = edges[i][1];
-
-            vector<bool> visited(n, false);
-            if (adj.find(u) != adj.end() && adj.find(v) != adj.end() && dfs(adj, u, v, visited)) {
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        //CONNECTED GRAPH : nodes-n, edges-n-1
+        //due to redundant connection here, 
+        //number of nodes=n
+        //number of edges=n 
+        int n=edges.size();   
+        
+        unordered_map<int , vector<int>> adj; //graph banana h so map
+        for(int i=0;i<n;i++){
+            vector<bool> visited(n,false); 
+            int u=edges[i][0];
+            int v=edges[i][1];
+            
+            if(adj.find(u)!=adj.end() && adj.find(v)!=adj.end() && dfs(adj,u,v,visited)){
                 return edges[i];
             }
-
-            adj[u].push_back(v);
+            adj[u].push_back(v); //otherwise draw graph
             adj[v].push_back(u);
         }
-
-        return {};
+        return {};             
     }
 };
