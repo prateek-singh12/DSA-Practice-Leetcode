bf45a58aabbd2479ef459ac1f2615c7041e26140
Prateek Singh <146692886+prateek-singh12@users.noreply.github.com>
2025-06-01 02:31:30 +0530

    Time: 24 ms (79.23%), Space: 23 MB (35.6%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,33 +1,75 @@
+// class Solution {
+// public:
+
+//     void dfs(vector<vector<char>>& grid, int i, int j){
+//         int m= grid.size(); //row
+//         int n= grid[0].size(); //col
+//         if(i<0 || i>=m || j<0 || j>=n || grid[i][j]!='1'){
+//             return;
+//         }
+//         grid[i][j]='v'; //mark visited
+//         dfs(grid,i+1,j);
+//         dfs(grid,i-1,j);
+//         dfs(grid,i,j+1);
+//         dfs(grid,i,j-1);
+//     }
+
+//     int numIslands(vector<vector<char>>& grid) {
+
+//         int m= grid.size(); //row
+//         int n= grid[0].size(); //col
+//         int ans=0;
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 if(grid[i][j]=='1'){
+//                     dfs(grid,i,j);
+//                     ans++;
+//                 }
+//             }
+//         }
+//         return ans;
+        
+//     }
+// };
+
+//bfs
 class Solution {
 public:
+    int numIslands( vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int count = 0;
 
-    void dfs(vector<vector<char>>& grid, int i, int j){
-        int m= grid.size(); //row
-        int n= grid[0].size(); //col
-        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]!='1'){
-            return;
-        }
-        grid[i][j]='v'; //mark visited
-        dfs(grid,i+1,j);
-        dfs(grid,i-1,j);
-        dfs(grid,i,j+1);
-        dfs(grid,i,j-1);
-    }
+        vector<pair<int, int>> directions = {
+            {1, 0}, {-1, 0}, {0, 1}, {0, -1}
+        };
 
-    int numIslands(vector<vector<char>>& grid) {
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == '1') {
+                    ++count;
+                    queue<pair<int, int>> q;
+                    q.push({i, j});
+                    grid[i][j] = 'v'; // Mark as visited
 
-        int m= grid.size(); //row
-        int n= grid[0].size(); //col
-        int ans=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]=='1'){
-                    dfs(grid,i,j);
-                    ans++;
+                    while (!q.empty()) {
+                        auto [x, y] = q.front();
+                        q.pop();
+
+                        for (auto [dx, dy] : directions) {
+                            int nx = x + dx;
+                            int ny = y + dy;
+
+                            if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == '1'){
+                                q.push({nx, ny});
+                                grid[nx][ny] = 'v'; // Mark as visited
+                            }
+                        }
+                    }
                 }
             }
         }
-        return ans;
-        
+
+        return count;
     }
 };
