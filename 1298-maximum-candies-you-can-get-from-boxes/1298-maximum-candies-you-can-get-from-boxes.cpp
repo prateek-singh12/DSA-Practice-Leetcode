c1860eb0bb18007bf1982e0ba4124cf1100fb2b7
Prateek Singh <146692886+prateek-singh12@users.noreply.github.com>
2025-06-03 22:12:36 +0530

    Time: 16 ms (20.67%), Space: 45.4 MB (16.83%) - LeetHub

---
+++ 1298-maximum-candies-you-can-get-from-boxes/1298-maximum-candies-you-can-get-from-boxes.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
+        
+        int candiesCollected = 0;
+        unordered_set<int> visited;
+        unordered_set<int> foundBoxes;
+       
+        queue<int> que; 
+        for(int &box : initialBoxes) {
+            foundBoxes.insert(box);
+            if(status[box] == 1) {
+                que.push(box);
+                visited.insert(box);
+                candiesCollected += candies[box];
+            }
+        }
+
+        while(!que.empty()) {
+            int box = que.front();
+            que.pop();
+
+            for(int &insideBox : containedBoxes[box]) {
+                foundBoxes.insert(insideBox);
+                if(status[insideBox] == 1 && !visited.count(insideBox)) {
+                    que.push(insideBox);
+                    visited.insert(insideBox);
+                    candiesCollected += candies[insideBox];
+                }
+            }
+
+            for(int &boxKey : keys[box]) {
+                status[boxKey] = 1; 
+                if(foundBoxes.count(boxKey) && !visited.count(boxKey)) {
+                    que.push(boxKey);
+                    visited.insert(boxKey);
+                    candiesCollected += candies[boxKey];
+                }
+            }
+        }
+
+        return candiesCollected;
+    }
+};
