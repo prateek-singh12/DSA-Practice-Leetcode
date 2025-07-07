3c4e06796407ee9fe2cf27a99cbc83cc4b217651
Prateek Singh <146692886+prateek-singh12@users.noreply.github.com>
2025-07-07 12:29:16 +0530

    Time: 99 ms (21.4%), Space: 106.5 MB (22.27%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     static bool compareByEnd(vector<int>& a, vector<int>& b) {
-        return a[1] < b[1];
+        return a[1] < b[1]; //ascending order of ending time
     }
 
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        if (intervals.empty()) return 0;
 
         sort(intervals.begin(), intervals.end(), compareByEnd);
 
         int count = 0;
         int end = INT_MIN;
-
+        
         for (auto interval : intervals) {
             if (interval[0] >= end) {
                 end = interval[1];
-                count++;
+                count++;//feasuble or correct events //return total -count
             }
         }
 
