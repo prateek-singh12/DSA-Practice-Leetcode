751fc1ccf3d26213da6eb2bc562d0d1ae9894e4d
Prateek Singh <146692886+prateek-singh12@users.noreply.github.com>
2025-07-11 08:39:30 +0530

    Time: 4 ms (17.02%), Space: 9.9 MB (58.34%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,20 +1,45 @@
 class Solution {
- public:
-  bool isPalindrome(string s) {
-    int l = 0;
-    int r = s.length() - 1;
-
-    while (l < r) {
-      while (l < r && !isalnum(s[l]))
-        ++l;
-      while (l < r && !isalnum(s[r]))
-        --r;
-      if (tolower(s[l]) != tolower(s[r]))
-        return false;
-      ++l;
-      --r;
+public:
+    bool isPalindrome(string s) {
+        int l = 0;
+        int r = s.size() - 1;
+        while (l < r) {
+            if (!isalnum(s[l])) {//Skip non-alphanumeric characters
+                ++l;
+                continue;
+            }
+            if (!isalnum(s[r])) {
+                --r;
+                continue;
+            }
+            if (tolower(s[l]) != tolower(s[r]))
+                return false;
+            ++l;
+            --r;
+        }
+        return true;
     }
-
-    return true;
-  }
 };
+
+
+
+// class Solution {
+//  public:
+//   bool isPalindrome(string s) {
+//     int l = 0;
+//     int r = s.length() - 1;
+
+//     while (l < r) {
+//       while (l < r && !isalnum(s[l]))
+//         ++l;
+//       while (l < r && !isalnum(s[r]))
+//         --r;
+//       if (tolower(s[l]) != tolower(s[r]))
+//         return false;
+//       ++l;
+//       --r;
+//     }
+
+//     return true;
+//   }
+// };
