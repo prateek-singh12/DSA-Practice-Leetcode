999760791aa61082cc98be4b2b3b2877dcdc9355
Prateek Singh <146692886+prateek-singh12@users.noreply.github.com>
2025-07-11 11:25:17 +0530

    Time: 0 ms (100%), Space: 14.8 MB (36.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,21 +1,48 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> numMap;
-        vector<int> ans;
-        
-        for (int i = 0; i < nums.size(); i++) {
-            int complement = target - nums[i];
-            
-            //if complement exists in the map
-            if (numMap.find(complement) != numMap.end()) {
-                ans.push_back(numMap[complement]);
-                ans.push_back(i);
-                return ans;
-            }           
-            numMap[nums[i]] = i;
+        vector<int> res;
+       //sort(nums.begin(),nums.end()); //sorting the nums array in-place, but after sorting, you lose the original indices of the elements.
+        vector<pair<int,int>> clone;
+        for(int i=0;i<nums.size();i++){
+            clone.push_back({nums[i],i});
         }
-        
-        return ans;
+       sort(clone.begin(),clone.end());
+       int l=0;
+       int r=nums.size()-1;
+       while(l<=r){
+            if(clone[l].first+clone[r].first==target){
+                    res.push_back(clone[l].second);
+                    res.push_back(clone[r].second);
+                    return res;
+                } 
+            else if(clone[l].first+clone[r].first>target){r--;}
+            else{l++;}
+       }
+       return res;
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         unordered_map<int, int> numMap;
+//         vector<int> ans;
+        
+//         for (int i = 0; i < nums.size(); i++) {
+//             int complement = target - nums[i];
+            
+//             //if complement exists in the map
+//             if (numMap.find(complement) != numMap.end()) {
+//                 ans.push_back(numMap[complement]);
+//                 ans.push_back(i);
+//                 return ans;
+//             }           
+//             numMap[nums[i]] = i;
+//         }
+        
+//         return ans;
+//     }
+// };
+
