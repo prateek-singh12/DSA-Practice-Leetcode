64ec716a749aab96b6b3fc4446c279317597b69e
Prateek Singh <146692886+prateek-singh12@users.noreply.github.com>
2025-07-14 10:59:00 +0530

    Time: 0 ms (100%), Space: 10.4 MB (93.31%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -8,25 +8,34 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     int getDecimalValue(ListNode* head) {
+//         int num=0;
+//         while(head!=NULL){
+//             num= num*10 + head->val;
+//             head=head->next;
+//         }
+//         int ans=0;
+//         int temp=0,p=0;
+//         while(num>0){
+//             temp=(num%10)*pow(2,p);
+//             ans=ans+temp;
+//             p++;
+//             num=num/10;
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-
     int getDecimalValue(ListNode* head) {
-        long long num=0;
-        ListNode* tempp=head;
-        while(tempp!=NULL){
-            num= (long long)num*10 + (long long)tempp->val;
-            tempp=tempp->next;
+        int num = 0;
+        while (head != NULL) {
+            num = (num << 1) | head->val;  // Left shift and add the next binary digit
+            head = head->next;
         }
-
-        int ans=0;
-        int temp=0,p=0;
-        while(num>0){
-            temp=(num%10)*pow(2,p);
-            ans=ans+temp;
-            p++;
-            num=num/10;
-        }
-        return ans;
+        return num;
     }
 };
