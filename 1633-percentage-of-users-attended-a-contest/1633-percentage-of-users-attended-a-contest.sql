ad2e40e33ef734197644151b89977621b0eeb7f2
Prateek Singh <146692886+prateek-singh12@users.noreply.github.com>
2025-07-24 08:14:23 +0530

    Time: 2874 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -1,7 +1,15 @@
-# Write your MySQL query statement below
-select B.contest_id, round((count(B.user_id)/(select count(*) from Users))*100,2) as percentage 
-from Users A, Register B
-where A.user_id=B.user_id
-group by B.contest_id
-order by percentage DESC, B.contest_id ASC;
--- Return the result table ordered by percentage in descending order. In case of a tie, order it by contest_id in ascending order.
+SELECT 
+    r.contest_id,
+    ROUND(COUNT(DISTINCT r.user_id) * 100.0 / COUNT(DISTINCT u.user_id), 2) AS percentage
+FROM Register r
+JOIN Users u
+GROUP BY r.contest_id
+ORDER BY percentage DESC, r.contest_id ASC;
+
+
+-- select B.contest_id, round((count(B.user_id)/(select count(*) from Users))*100,2) as percentage 
+-- from Users A, Register B
+-- where A.user_id=B.user_id
+-- group by B.contest_id
+-- order by percentage DESC, B.contest_id ASC;
+-- -- Return the result table ordered by percentage in descending order. In case of a tie, order it by contest_id in ascending order.
