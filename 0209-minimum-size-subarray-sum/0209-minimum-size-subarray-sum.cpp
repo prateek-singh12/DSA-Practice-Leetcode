9cc90227bf1cbd3780fa2546e7bc1d2dca16b13d
Prateek Singh <146692886+prateek-singh12@users.noreply.github.com>
2025-07-27 17:45:26 +0530

    Time: 0 ms (100%), Space: 32 MB (96.76%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        //Subarray is a contiguous non-empty sequence CONTIGUOUS - THINK OF SLIDING WINDOW 
+        
         int n=nums.size();
         int i=0,j=0,sum=0;
-        int minlength=INT_MAX; //answer
-        for(int k=0;k<n;k++){
-            sum=sum+nums[j];
-            while(sum>=target){ //then decrease window size WHILE LOOP
-                minlength=min(minlength,j-i+1); //update ans
-                sum=sum-nums[i]; //trying to decreasde window size by i++
+        int res=INT_MAX; //answer
+        int currlength=0;
+
+        while(j<n){
+
+            
+
+            sum += nums[j];  // expanding window
+
+            while (sum >= target) {  // shrinking window
+                res = min(res, j - i + 1);  // update answer
+                sum -= nums[i];
                 i++;
             }
+
             j++;
-        }
-        return minlength == INT_MAX ? 0 : minlength;  //IF no subarray, return 0      
+        }  
+        if(res==INT_MAX) return 0;  //sum of all elements smaller than target
+        return res; 
     }
 };
