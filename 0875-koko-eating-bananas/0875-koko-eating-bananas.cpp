04ac1ea7a5bd78a0030469d60acb876a62972d66
Prateek Singh <146692886+prateek-singh12@users.noreply.github.com>
2025-07-28 12:33:42 +0530

    Time: 11 ms (41.79%), Space: 22.8 MB (73.73%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,26 +1,25 @@
 class Solution {
- public:
-  int minEatingSpeed(vector<int>& piles, int h) {
-    int l = 1;
-    int r = ranges::max(piles);
-
-    while (l < r) {
-      const int m = (l + r) / 2;
-      if (eatHours(piles, m) <= h)
-        r = m;
-      else
-        l = m + 1;
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l=1;
+        int r=*max_element(piles.begin(), piles.end());
+        //taking hours from 0 to max element        
+        while(l<r){
+            int mid=l+(r-l)/2;
+            int hour=0;
+            for(int bananas: piles){
+                hour+=bananas/mid;
+                if(bananas%mid != 0) //not divisible -> for taking ceil add ++
+                    hour++;
+            }
+            if(hour<=h){                
+                r=mid;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return l;
     }
-
-    return l;
-  }
-
- private:
-  // Returns the hours to eat all the piles with speed m.
-  int eatHours(const vector<int>& piles, int m) {
-    return accumulate(piles.begin(), piles.end(), 0,
-                      [&](int subtotal, int pile) {
-      return subtotal + (pile - 1) / m + 1;  // ceil(pile / m)
-    });
-  }
 };
+
