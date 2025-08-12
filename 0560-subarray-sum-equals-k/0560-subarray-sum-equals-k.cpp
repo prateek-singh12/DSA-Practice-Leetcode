0eed1e7a9ab944e26c9d7f5462a898856ec09f99
Prateek Singh <146692886+prateek-singh12@users.noreply.github.com>
2025-08-12 22:35:00 +0530

    Time: 51 ms (51.56%), Space: 51.1 MB (17.71%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,18 +1,60 @@
+// class Solution {
+// public:
+//     int subarraySum(vector<int>& arr, int k) {
+//     //we find out, that a subarray may also exist if this particular condition satisified, 
+//     //(prefix[i] - k) should exist in our map, 
+//     //If prefix[i] - prefix[j] = k where i > j 
+//     //i.e if we are able to find (prefix[i] - k) is exist in our map, i.e prefix[j]
+// 	//so, we can say that from index j + 1 to i, the sum of the elements is k,
+//         int n = arr.size(); 
+//         vector<int> prefix(n); // prefix array        
+//         prefix[0] = arr[0];  //starting for loop from 1 so initialize for 0 index        
+//         for(int i = 1; i < n; i++)
+//         {
+//             prefix[i] = arr[i] + prefix[i - 1];
+//         }
+
+//         unordered_map<int,int> mp;
+//         int ans = 0;         
+//         for(int i = 0; i < n; i++) 
+//         {
+//             if(prefix[i] == k) // if it already becomes equal to k
+//                 ans++;
+//             // find whether (prefix[i] - k) present in map or not
+//             if(mp.find(prefix[i] - k) != mp.end())
+//             {
+//                 ans += mp[prefix[i] - k];
+//             }
+            
+//             mp[prefix[i]]++; 
+//         }
+        
+//         return ans; 
+//     }
+// };
+
+
+
 class Solution {
- public:
-  int subarraySum(vector<int>& nums, int k) {
-    int ans = 0;
-    int prefix = 0;
-    unordered_map<int, int> count{{0, 1}};  // {prefix sum: count}
-
-    for (const int num : nums) {
-      prefix += num;
-      const int target = prefix - k;
-      if (const auto it = count.find(target); it != count.cend())
-        ans += it->second;
-      ++count[prefix];
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> prefix;
+        prefix[0]=1;
+        int cursum=0;
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            cursum+=nums[i];
+            
+            
+                count=count+prefix[cursum-k];
+            
+            prefix[cursum]++;
+        }
+        return count;
     }
-
-    return ans;
-  }
 };
+
+
+
+
+
