237afc508dbe68adc1d990d42fdb3526b4521f52
Prateek Singh <146692886+prateek-singh12@users.noreply.github.com>
2025-08-26 23:13:32 +0530

    Time: 10 ms (25.6%), Space: 21 MB (12.16%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,46 +1,85 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-
         vector<vector<int>> adj(numCourses);
-        //adjacency list
-        for (auto i : prerequisites) {
-            adj[i[0]].push_back(i[1]);
+        for(auto i: prerequisites){
+            int u=i[0];
+            int v=i[1];
+            adj[u].push_back(v);
         }
-
-        vector<int> indegree(numCourses);
-        for(auto it:prerequisites){
-            indegree[it[1]]++;
+        vector<int> indegree(numCourses,0);
+        for(auto i: prerequisites){
+            indegree[i[1]]++;
         }
-        // for (int i = 0; i < numCourses; i++) {
-        //     for (auto it : adj[i]) {
-        //         indegree[it]++;
-        //     }
-        // }
-
-        queue<int> q;
-        for(int i=0;i<numCourses;i++){
+        queue<int>q;
+        for(int i=0;i<indegree.size();i++){
             if(indegree[i]==0){
                 q.push(i);
             }
         }
         vector<int> toposort;
+
         while(!q.empty()){
             int node=q.front();
             q.pop();
             toposort.push_back(node);
-            for(auto it : adj[node]){
-                indegree[it]--;
-                if(indegree[it]==0){
-                    q.push(it);
+            for(auto ngbr : adj[node]){
+                indegree[ngbr]--;
+                if(indegree[ngbr]==0){
+                    q.push(ngbr);
                 }
             }
         }
-        if(toposort.size()==numCourses){
-            return true;
-        }
+        if(toposort.size()==numCourses) return true;
         return false;
 
-        
     }
 };
+
+
+// class Solution {
+// public:
+//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+
+//         vector<vector<int>> adj(numCourses);
+//         //adjacency list
+//         for (auto i : prerequisites) {
+//             adj[i[0]].push_back(i[1]);
+//         }
+
+//         vector<int> indegree(numCourses);
+//         for(auto it:prerequisites){
+//             indegree[it[1]]++;
+//         }
+//         // for (int i = 0; i < numCourses; i++) {
+//         //     for (auto it : adj[i]) {
+//         //         indegree[it]++;
+//         //     }
+//         // }
+
+//         queue<int> q;
+//         for(int i=0;i<numCourses;i++){
+//             if(indegree[i]==0){
+//                 q.push(i);
+//             }
+//         }
+//         vector<int> toposort;
+//         while(!q.empty()){
+//             int node=q.front();
+//             q.pop();
+//             toposort.push_back(node);
+//             for(auto it : adj[node]){
+//                 indegree[it]--;
+//                 if(indegree[it]==0){
+//                     q.push(it);
+//                 }
+//             }
+//         }
+//         if(toposort.size()==numCourses){
+//             return true;
+//         }
+//         return false;
+
+        
+//     }
+// };
